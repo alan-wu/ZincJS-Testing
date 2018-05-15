33491f38be9711e18bc9d34af7a415d03947c0f3
alan-wu <alan.wu@auckland.ac.nz>
2018-05-15 14:15:40 +1200

    Add testing for zinc.controls.

--- test/test.js
+++ test/test.js
@@ -8,7 +8,6 @@ const dom = new JSDOM(`<body><div id="container" styles="width:1024px;height:102
 const container = dom.window.document.querySelector("#container");
 global.window = dom.window;
 global.document = dom.window.document;
-var testScene = null;
 global.XMLHttpRequest = require("local-xmlhttprequest").XMLHttpRequest;
 
 var testBoxGeometry = new THREE.BoxGeometry( 10, 10, 10 );
@@ -37,10 +36,255 @@ var geometryCallback = function(done) {
   }
 }
 
+var loadPathCallback = function(done) {
+  return function() { 
+    done();
+  }
+}
+
+function checkControls(scene) {
+  describe('Controls()', function(){
+    var controls = undefined;
+    before('Get Camera Controls', function() {
+      controls = scene.getZincCameraControls();
+      assert.isObject(controls, 'controls is available');
+    });
+    describe('Local Variables()', function(){
+      it('cameraObject', function(){
+        assert.isObject(controls.cameraObject, 'cameraObject is an object');
+      });
+    //  it('domElement', function(){
+    //    assert.isObject(controls.domElement, 'domElement is an object');
+    //  });
+      it('renderer', function(){
+        assert.isObject(controls.renderer, 'renderer is an object');
+      });
+      it('scene', function(){
+        assert.isObject(controls.scene, 'renderer is an object');
+      });
+      it('tumble_rate', function(){
+        assert.equal(controls.tumble_rate, 1.5, 'tumble_rate is correct');
+      });
+      it('scrollRate', function(){
+        assert.equal(controls.scrollRate, 50, 'scrollRate is correct');
+      });
+    });
+    describe('Methods()', function(){
+      it('setMouseButtonAction', function(){
+        assert.isUndefined(controls.setMouseButtonAction("AUXILIARY", "ZOOM"), 'setMouseButtonAction is successfully called');
+      });
+      it('updateDirectionalLight', function(){
+        assert.isUndefined(controls.updateDirectionalLight(), 'updateDirectionalLight is successfully called');
+      });
+      it('disable', function(){
+        assert.isUndefined(controls.updateDirectionalLight(), 'disable is successfully called');
+      });
+      it('enable', function(){
+        assert.isUndefined(controls.enable(""), 'enable is successfully called');
+      });
+      it('setPathDuration', function(){
+        assert.isUndefined(controls.setPathDuration(2000), 'setPathDuration is successfully called');
+      });
+      it('setTime', function(){
+        assert.isUndefined(controls.setTime(1000), 'setTime is successfully called');
+      });
+      it('setPlayRate', function(){
+        assert.isUndefined(controls.setPlayRate(200), 'setPlayRate is successfully called');
+      });
+      it('loadPathURL', function(done){
+        assert.isUndefined(controls.loadPathURL('models/test_path.json', loadPathCallback(done)),
+        'loadPathURL is successfully called');
+      });
+      it('getNumberOfTimeFrame', function(){
+        assert.equal(controls.getNumberOfTimeFrame(), 8135, 'getNumberOfTimeFrame returns the correct value');
+      });
+      it('getCurrentTimeFrame', function(){
+        assert.equal(controls.getCurrentTimeFrame()[0], 4067, 'getCurrentTimeFrame returns the correct value');
+      });
+      it('setCurrentTimeFrame', function(){
+        assert.isUndefined(controls.setCurrentTimeFrame(4110), 'setCurrentTimeFrame is successfully called');
+        assert.equal(controls.getCurrentTimeFrame()[0], 4110, 'getCurrentTimeFrame returns the correct value');
+      });
+      it('playPath', function(){
+        assert.isUndefined(controls.playPath(), 'playPath is successfully called');
+        assert.isTrue(controls.isPlayingPath(), 'Playing path');
+      });
+      it('update', function(){
+        assert.isUndefined(controls.update(1), 'update is successfully called');
+        assert.equal(controls.getCurrentTimeFrame()[0], 4923, 'getCurrentTimeFrame returns the correct value');
+      });
+      it('stopPath', function(){
+        assert.isUndefined(controls.stopPath(), 'stopPath is successfully called');
+        assert.isFalse(controls.isPlayingPath(), 'Stop playing path');
+      });
+      it('enableDirectionalLightUpdateWithPath', function(){
+        assert.isUndefined(controls.enableDirectionalLightUpdateWithPath(true),
+          'enableDirectionalLightUpdateWithPath is successfully called');
+      });
+      it('enableDeviceOrientation', function(){
+        assert.isUndefined(controls.enableDeviceOrientation(), 'enableDeviceOrientation is successfully called');
+        assert.isTrue(controls.isDeviceOrientationEnabled(), 'Device Orientation is correctly enabled');
+      });
+      it('disableDeviceOrientation', function(){
+        assert.isUndefined(controls.disableDeviceOrientation(), 'disableDeviceOrientation is successfully called');
+        assert.isFalse(controls.isDeviceOrientationEnabled(), 'Device Orientation is correctly disable');
+      });
+      it('getViewportFromCentreAndRadius', function(){
+        var viewport = controls.getViewportFromCentreAndRadius(10,20,30, 30, 40, 100);
+        assert.isObject(viewport, 'getViewportFromCentreAndRadius successfully returns an object');
+      });
+      it('getDefaultViewport', function(){
+        var viewport = controls.getDefaultViewport();
+        assert.isObject(viewport, 'getDefaultViewport successfully returns an object');
+      });
+      it('getCurrentViewport', function() {
+        var viewport = controls.getCurrentViewport();
+        assert.isObject(viewport, 'getCurrentViewport successfully returns an object');
+      });
+      it('setDefaultCameraSettings', function(){
+        var viewport = controls.getViewportFromCentreAndRadius(10,20,30, 30, 40, 100);
+        assert.isObject(viewport, 'getViewportFromCentreAndRadius successfully returns an object');
+        assert.isUndefined(controls.setDefaultCameraSettings(viewport), 'setDefaultCameraSettings  successfully called');
+        assert.equal(viewport.eyePosition[0], controls.getDefaultViewport().eyePosition[0], 'Default camera settings are set correctly');
+      });
+      it('setCurrentCameraSettings', function(){
+        var viewport = controls.getViewportFromCentreAndRadius(10,20,30, 30, 40, 100);
+        assert.isObject(viewport, 'getViewportFromCentreAndRadius successfully returns an object');
+        assert.isUndefined(controls.setCurrentCameraSettings(viewport), 'setCurrentCameraSettings is successfully called');
+        assert.equal(viewport.eyePosition[0], controls.getCurrentViewport().eyePosition[0], 'Current camera settings are set correctly');
+      });
+      it('resetView', function(){
+        assert.isUndefined(controls.resetView(), 'resetView is successfully called');
+      });
+      it('cameraTransition', function(){
+        var startingViewport = controls.getViewportFromCentreAndRadius(10,20,30, 30, 40, 100);
+        var endingViewport = controls.getViewportFromCentreAndRadius(20,40,60, 60, 80, 200);
+        assert.isUndefined(controls.cameraTransition(startingViewport, endingViewport, 2000), 
+            'cameraTransition is successfully called');
+      });
+      it('enableCameraTransition', function(){
+        assert.isUndefined(controls.enableCameraTransition(), 'enableCameraTransition is successfully called');
+        assert.isTrue(controls.isTransitioningCamera(), 'camera transition is successfully enabled');
+      });
+      it('pauseCameraTransition', function(){
+        assert.isUndefined(controls.pauseCameraTransition(), 'pauseCameraTransition is successfully called');
+        assert.isFalse(controls.isTransitioningCamera(), 'camera transition is successfully paused');
+      });
+      it('stopCameraTransition', function(){
+        assert.isUndefined(controls.stopCameraTransition(), 'stopCameraTransition is successfully called');
+        assert.isFalse(controls.isTransitioningCamera(), 'camera transition is successfully stopped');
+      });
+      it('autoTumble', function(){
+        assert.isUndefined(controls.autoTumble([1.0, 0.0], 0.01, true), 'autoTumble is successfully called');
+      });
+      it('enableAutoTumble', function(){
+        assert.isUndefined(controls.enableAutoTumble(), 'enableAutoTumble is successfully called');
+        assert.isTrue(controls.isAutoTumble(), 'auto tumble is successfully enabled');
+      });
+      it('updateAutoTumble', function(){
+        assert.isUndefined(controls.updateAutoTumble(), 'updateAutoTumble is successfully called');
+      });
+      it('stopAutoTumble', function(){
+        assert.isUndefined(controls.stopAutoTumble(), 'stopAutoTumble is successfully called');
+        assert.isFalse(controls.isAutoTumble(), 'auto tumble is successfully stopped');
+      });
+      it('enableRaycaster', function(){
+        assert.isUndefined(controls.enableRaycaster(scene, undefined, undefined), 'enableRaycaster is successfully called');
+      });
+      it('disableRaycaster', function(){
+        assert.isUndefined(controls.disableRaycaster(), 'disableRaycaster is successfully called');
+      });
+    });
+  });
+}
+
+function checkGeometry(scene) {
+  describe('Geometry()', function(){
+    var geometry = undefined;
+    var id = 4001;
+    before('New Zinc Geometry', function() {
+      geometry = scene.addZincGeometry(testBoxGeometry, id, 0x00ff00, 1.0, false, false);
+      geometry.groupName = "TestGeometry";
+      assert.isObject(geometry, 'ZincGeometry has been created');
+      assert.equal(geometry.groupName, "TestGeometry", 'ZincGeometry group name has been set');
+    });
+    describe('Local Variables()', function(){
+      it('geometry', function(){
+        assert.isObject(geometry.geometry, 'geometry is an object');
+      });
+      it('mixer', function(){
+        assert.isObject(geometry.mixer, 'mixer is an object');
+      });
+      it('timeEnabled', function(){
+        assert.isFalse(geometry.timeEnabled, 'timeEnabled is false');
+      });
+      it('morphColour', function(){
+        assert.isFalse(geometry.morphColour, 'morphColour is an object');
+      });
+      it('modelId', function(){
+        assert.equal(geometry.modelId, id, 'modelId is correct');
+      });
+      it('morph', function(){
+        assert.isObject(geometry.morph, 'morph is an object');
+      });
+      it('clipAction', function(){
+        assert.isUndefined(geometry.clipAction, 'clipAction is an object');
+      });
+      it('groupName', function(){
+        assert.equal(geometry.groupName, "TestGeometry", 'groupName is correct');
+      });
+    });
+    describe('Methods()', function(){
+      it('setVisibility', function() {
+        assert.isUndefined(geometry.setVisibility(false), 'setVisibility is successfully called');
+        assert.isFalse(geometry.morph.visible, 'visibility is false');
+      });
+      it('setAlpha', function() {
+        assert.isUndefined(geometry.setAlpha(0.5), 'setAlpha is successfully called');
+        assert.isTrue(geometry.morph.material.transparent, 'transparent is true');
+        assert.equal(geometry.morph.material.opacity, 0.5, 'opacity is correct');
+      });
+      it('getCurrentTime', function() {
+        assert.equal(geometry.getCurrentTime(), 0.0, 'getCurrentTime returns the correct value');
+      });
+      it('setMorphTime', function() {
+        assert.isUndefined(geometry.setMorphTime(1500), 'setMorphTime is successfully called');
+        assert.equal(geometry.getCurrentTime(), 1500.0, 'getCurrentTime returns the correct value');
+      });
+      it('setWireframe', function() {
+        assert.isUndefined(geometry.setWireframe(true), 'setWireframe is successfully called');
+        assert.isTrue(geometry.morph.material.wireframe , 'wireframe is correct');
+      });
+      it('setColour', function() {
+        assert.isUndefined(geometry.setColour(0x888888), 'setColour is successfully called');
+        assert.equal(geometry.morph.material.color , 0x888888, 'colour is correct');
+      });
+      it('setVertexColour', function() {
+        assert.isUndefined(geometry.setVertexColors(THREE.NoColors), 'setVertexColour is successfully called');
+        assert.equal(geometry.morph.material.vertexColors , THREE.NoColors, 'colour is correct');
+      });
+      it('getBoundingBox', function() {
+        var boundingBox = geometry.getBoundingBox();
+        assert.isObject(boundingBox, 'boundingBox is successfully called');
+        assert.isObject(boundingBox.min,'boundingbox`s min is alright');
+        assert.isObject(boundingBox.max, 'boundingbox`s max is alright');
+      });
+      it('setMaterial', function() {
+        var material = new THREE.MeshBasicMaterial( {
+          color: 0x000000
+        } );
+        assert.isUndefined(geometry.setMaterial(material), 'setMaterial is successfully called');;
+        assert.equal(geometry.morph.material, material, 'material is correct');
+      });
+    });
+  });
+}
+
 function checkScene(renderer) {
-  var testGeometry = undefined;
+  var testScene = undefined;
   describe('Scene()', function(){
     var scene = renderer.createScene("TestScene");
+    testScene = scene;
     it('New scene object', function(){
       assert.isObject(scene, 'Scene has been created');
     });
@@ -54,6 +298,7 @@ function checkScene(renderer) {
       });
     });
     describe('Methods()', function(){
+      var testGeometry = undefined;
       var id = 3001;
       before('addZincGeometry', function() {
         testGeometry = scene.addZincGeometry(testBoxGeometry, id, 0x00ff00, 1.0, false, false);
@@ -104,6 +349,9 @@ function checkScene(renderer) {
       it('getZincGeometryByID', function(){
         assert.equal(scene.getZincGeometryByID(id), testGeometry, 'getZincGeometryByID returns the correct object');
       });
+      it('getZincCameraControls', function() {
+        assert.isObject(scene.getZincCameraControls(), 'getZincCameraControls returns the correct object');
+      });
       it('getThreeJSScene', function(){
         assert.isObject(scene.getThreeJSScene(), 'getThreeJSScene returns the correct object');
       });
@@ -116,8 +364,15 @@ function checkScene(renderer) {
       it('isStereoEffectEnable', function(){
         assert.isTrue(scene.isStereoEffectEnable(), 'setStereoEffectEnable is successfully called');
       });
+      it('removeZincGeometry', function(){
+        assert.isUndefined(scene.removeZincGeometry(testGeometry), 'removeZincGeometry is successfully called');
+      });
+      it('getZincGeometryByID', function(){
+        assert.isNull(scene.getZincGeometryByID(id), 'getZincGeometryByID returns the correct object');
+      });
     });
   });
+  return testScene;
 }
 
 function checkRenderer() {
@@ -221,7 +476,9 @@ function checkRenderer() {
     })
     testRenderer = renderer;
   })
-  checkScene(testRenderer);
+  var testScene = checkScene(testRenderer);
+  checkGeometry(testScene);
+  checkControls(testScene);
 }
 
 function checkZincObject() {
@@ -229,23 +486,6 @@ function checkZincObject() {
     assert.isObject(Zinc, 'Zinc is an object'); 
   });
   checkRenderer();
-}
-
-if (0) {
-  this.Geometry = require('./geometry').Geometry;
-  this.Glyph = require('./glyph').Glyph;
-  this.Glyphset = require('./glyphset').Glyphset;
-  this.Renderer = require('./renderer').Renderer;
-  this.Scene = require('./scene').Scene;
-  
-  this.Viewport = require('./controls').Viewport;
-  this.CameraControls = require('./controls').CameraControls;
-  this.SmoothCameraTransition = require('./controls').SmoothCameraTransition;
-  this.RayCaster = require('./controls').RayCaster;
-  this.CameraAutoTumble = require('./controls').CameraAutoTumble;
-  this.loadExternalFile = require('./utilities').loadExternalFile;
-  this.loadExternalFiles = require('./utilities').loadExternalFiles;
-  this.StereoEffect = require('./controls').StereoEffect;
 }
 
 describe('Zinc', function(){
